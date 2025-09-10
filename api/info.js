2ae11cff23fa1e645c5fb4ccc711820b628cfbb4
KAITOXcek <iansabida@gmail.com>
2025-09-10 19:05:51 +0800

    Add files via upload

---
+++ api/info.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    title: "My Sonolus Server",
+    banner: "https://example.com/banner.jpg",
+    background: "https://example.com/background.jpg",
+    description: "Welcome to my custom Sonolus server!",
+    genres: [],
+    servers: [],
+    recommended: []
+  });
+}
